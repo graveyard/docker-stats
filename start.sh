9188785c9a8a8619251fdb20257007a2fd384c79
Ryan Burns <ryan.burns@clever.com>
2016-04-21 17:09:16 -0700

    Add docker url

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ sed -e "s/<INFLUXDB_USER>/$INFLUXDB_USER/" \
     -e "s/<INFLUXDB_DB>/$INFLUXDB_DB/" \
     -e "s/<INFLUXDB_HOST>/$INFLUXDB_HOST/" \
     -e "s/<INFLUXDB_PORT>/$INFLUXDB_PORT/" \
+    -e "s/<DOCKER_HOST>/$DOCKER_HOST/" \
     /usr/bin/diamond.conf.template \
     > /etc/diamond/diamond.conf
 exec diamond -f --skip-change-user --skip-fork
